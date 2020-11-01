69d08254fcecc0c4bffc3c8d30447a01fdbb3759
emine-celik <emineclk80@hotmail.com>
2020-10-31 23:34:50 -0500

    first commit to the the Github

---
+++ AkbarDeneme1/src/main/java/Deneme1.java
@@ -0,0 +1,8 @@
+public class Deneme1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world" +
+                "Hello");
+
+        System.out.println("Hollowen");
+    }
+}
